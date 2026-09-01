400b4eebe0c882139c3063ea11178f13636ee515
Truls Henriksson <trulshenriksson@gmail.com>
2026-09-01 15:22:11 +0200

    Added useful methods for merging and querying Trees

--- src/typesnmethods.ts
+++ src/typesnmethods.ts
@@ -1,4 +1,4 @@
-// Helper for creating ID types that can't be mixed up with regular numbers
+// Helper for creating ID types that can't be mixed up with regular strings
 // https://stackoverflow.com/a/50521248
 type Opaque<T, K> = T & { __opaque__: K }
 
@@ -61,12 +61,45 @@ export class Person {
         return '';
     }
 
+    /** Return a copy with fields updated from `source`. */
+    mergedWith(source: Person){
+        let newPerson = this.copy()
+        // Take names from other unless they're empty
+        if (source.nameFirst !== "")
+            newPerson.nameFirst = source.nameFirst
+        if (source.nameLast !== "")
+            newPerson.nameLast = source.nameLast
+        if (source.nameLastMaiden !== "")
+            newPerson.nameLastMaiden = source.nameLastMaiden
+        // Take whichever date is more specific, assuming YYYY[-MM[-DD]] format
+        if (this.dateBirth.length < source.dateBirth.length)
+            newPerson.dateBirth = source.dateBirth
+        if (this.dateDeath.length < source.dateDeath.length)
+            newPerson.dateDeath = source.dateDeath
+        return newPerson
+    }
+
     asObject() {
         return Object.assign({}, this);
     }
 
     static fromObject(obj: IPerson): Person {
         return Object.assign(new Person(obj.id), obj);
+    }
+
+    copy(): Person {
+        return Object.assign(new Person(this.id), this)
+    }
+
+    /** Return how many of its fields are filled in, for sorting purposes. */
+    numFilledFields(): number {
+        return [
+            this.nameFirst !== "",
+            this.nameLast !== "",
+            this.nameLastMaiden !== "",
+            this.dateBirth !== "",
+            this.dateDeath !== "",
+        ].reduce((partialSum, a) => partialSum + (a ? 1 : 0), 0)
     }
 }
 
@@ -107,6 +140,24 @@ export class Family {
         return `(${this.nameLastOverride || husband?.nameLast || wife?.nameLast || '?'}) ${husbandName ?? '?'} + ${wifeName ?? '?'}${childrenNames.length > 0 ? ' = ' + childrenNames.join(', ') : ''}`;
     }
 
+    /** Return a copy with fields updated from `source`. */
+    mergedWith(source: Family) {
+        let newFamily = this.copy()
+        // Take fields from source, unless they're empty
+        if (source.husband !== null)
+            newFamily.husband = source.husband
+        if (source.wife !== null)
+            newFamily.wife = source.wife
+        // Deduplicate the combined children
+        newFamily.children = [...new Set(this.children.concat(source.children))]
+        if (source.nameLastOverride !== "")
+            newFamily.nameLastOverride = source.nameLastOverride
+        // Take whichever date is more specific, assuming YYYY[-MM[-DD]] format
+        if (this.dateStart.length < source.dateStart.length)
+            newFamily.dateStart = source.dateStart
+        return newFamily
+    }
+
     asObject() {
         return Object.assign({}, this);
     }
@@ -114,15 +165,36 @@ export class Family {
     static fromObject(obj: IFamily): Family {
         return Object.assign(new Family(obj.id), obj);
     }
+
+    copy(): Family {
+        return Object.assign(new Family(this.id), this)
+    }
 }
 
 export class Tree {
     people: Person[];
     families: Family[];
+    /** Map from a person to the families they are a parent of. */
+    private familyMap: Map<PersonId, FamilyId[]>
+    /** Map from a person to their parents: `[father, mother]`. */
+    private parentMap: Map<PersonId, [PersonId | null, PersonId | null]>
 
     constructor(people: Person[] = [], families: Family[] = []) {
         this.people = people;
         this.families = families;
+
+        // Initialize the family and parent maps
+        this.familyMap = new Map()
+        this.parentMap = new Map()
+        for (const family of this.families) {
+            if (family.husband !== null)
+                mapAppend(this.familyMap, family.husband, family.id)
+            if (family.wife !== null)
+                mapAppend(this.familyMap, family.wife, family.id)
+            for (const childId of family.children) {
+                this.parentMap.set(childId, [family.husband, family.wife])
+            }
+        }
     }
 
     addEmptyPerson(): PersonId {
@@ -170,14 +242,94 @@ export class Tree {
     ) {
         let family = this.getFamily(familyId);
 
-        switch (role) {
-            case 'husband':
-                family.husband = personId;
-            case 'wife':
-                family.husband = personId;
-            case 'child':
-                if (personId !== null) family.children.push(personId);
+        if (role === "child") {
+            if (personId !== null) {
+                family.children.push(personId)
+                this.parentMap.set(personId, [family.husband, family.wife])
+            }
+            return
         }
+
+        if (role === "husband") {
+            if (family.husband !== null) {
+                // De-register the current husband
+                mapRemove(this.familyMap, family.husband, familyId)
+                for (const childId of family.children) {
+                    const mother = this.parentMap.get(childId)![1];
+                    this.parentMap.set(childId, [null, mother])
+                }
+            }
+            family.husband = personId
+        } else {
+            if (family.wife !== null) {
+                // De-register the current wife
+                mapRemove(this.familyMap, family.wife, familyId)
+                for (const childId of family.children) {
+                    const father = this.parentMap.get(childId)![0];
+                    this.parentMap.set(childId, [father, null])
+                }
+            }
+            family.wife = personId
+        }
+
+        // Link this family to the given person
+        if (personId !== null)
+            mapAppend(this.familyMap, personId, familyId)
+
+        // Update the children's parents
+        for (const childId of family.children) {
+            this.parentMap.set(childId, [family.husband, family.wife])
+        }
+    }
+
+    getFamiliesFromParent(personId: PersonId): Family[] {
+        return (this.familyMap.get(personId) ?? []).map((id) => this.getFamily(id))
+    }
+
+    getSpousesFromParent(personId: PersonId): Person[] {
+        return (
+            this.getFamiliesFromParent(personId)
+                .map((f) => f.husband === personId ? f.wife : f.husband)
+                .filter((id) => id !== null)
+                .map((id) => this.getPerson(id))
+        )
+    }
+
+    getChildrenFromParent(personId: PersonId): Person[] {
+        return this.getFamiliesFromParent(personId).flatMap((f) => f.children.map(this.getPerson))
+    }
+
+    getParentsFromChild(personId: PersonId): [Person | undefined, Person | undefined] {
+        if (!this.parentMap.has(personId)) {
+            return [undefined, undefined]
+        }
+        let [fatherId, motherId] = this.parentMap.get(personId)!
+        return [
+            fatherId === null ? undefined : this.getPerson(fatherId),
+            motherId === null ? undefined : this.getPerson(motherId),
+        ]
+    }
+
+    /** Return siblings and half-siblings as two lists. */
+    getSiblings(personId: PersonId): [Person[], Person[]] {
+        let siblings: Set<PersonId> = new Set()
+        let halfSiblings: Set<PersonId> = new Set()
+        for (const parent of this.getParentsFromChild(personId)) {
+            if (parent === undefined)
+                continue
+            const families = this.getFamiliesFromParent(parent.id)
+            for (const family of families) {
+                let siblingSet = family.children.includes(personId) ? siblings : halfSiblings
+                for (const childId of family.children) {
+                    if (childId !== personId)
+                        siblingSet.add(childId)
+                }
+            }
+        }
+        return [
+            [...siblings].map(id => this.getPerson(id)),
+            [...halfSiblings].map(id => this.getPerson(id)),
+        ]
     }
 
     asObject(): Object {
@@ -273,4 +425,23 @@ export function familyIdFromString(str: string | null): FamilyId | null {
     if (str === null || str === '' || str === 'null') 
         return null;
     return str as FamilyId;
+}
+
+/** Append `value` to the list in `map.get(key)`. */
+export function mapAppend<K, V>(map: Map<K, V[]>, key: K, value: V) {
+    if (map.has(key)) {
+        map.get(key)!.push(value)
+    } else {
+        map.set(key, [value])
+    }
+}
+
+/** Remove `value` from the list in `map.get(key)`. */
+export function mapRemove<K, V>(map: Map<K, V[]>, key: K, value: V) {
+    let values = map.get(key) ?? []
+    let i = values.findIndex(v => v === value)
+    if (i !== -1) {
+        values.splice(i, 1)
+        map.set(key, values)
+    }
 }
